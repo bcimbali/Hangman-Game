1e07168e3cc66a458801e0a672df631adf2bb89e
bcimbali <bcimbali@gmail.com>
2018-03-02 17:04:25 -0600

    got multiple letters to appear in user guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,33 +19,56 @@ let guessedLetters = document.getElementById("already-guessed");
 let computerArray = Array.from(computerChoice);
 // Set a guessIndex of -1 so that it's always smaller than a value in an array (i.e., 0).
 let guessIndex = -1;
-
-// document.getElementById("wins").innerHTML = guesses_remaining;
-// let letterBlanks = document.getElementById("blanks");
-// document.getElementById("blanks").innerHTML = "Test";
-
-// ---------------------------------------------- CREATE FUNCTIONS ----------------------------------------------------
-
-// When the window loads, set the number of blanks
-window.onload = function() {
-    let numberSpaces = computerArray.fill("_");
-    document.getElementById("blanks").innerHTML = numberSpaces;
+//  Create a variable called blanksGuess and set it to an empty array - this will hold our blanks for the computer word.
+let blanksGuess = [];
+// Set the number of blanks equal to the length of the word.
+for (var i=0; i < computerChoice.length; i++) {
+    blanksGuess.push("_");
 }
 
+let correctArray = [];
+// Create a string from the blank guess array and join them together with a white space in between.
+blanksGuessStr = blanksGuess.join(" ");
+// Go get the HTML element with the id of blanks and set it to that blanksGuessStr.
+document.getElementById("blanks").innerHTML = blanksGuessStr; 
+
+
+// ---------------------------------------------- CREATE FUNCTIONS ----------------------------------------------------
 
 document.onkeyup = function(event) {
     // On the key up, set the variable userText to the key content of the event.
     userText = event.key;
     // Add the key value of the event to the empty lettersGuessed array
     lettersGuessed.push(userText);
+    document.getElementById("already-guessed").innerHTML = lettersGuessed;
     // In the DOM, get the already-guessed id and set it to the lettersGuessed array.
-    document.getElementById("already-guessed").innerHTML = lettersGuessed;    
+    
+    // An if statement that checks whether the key the user presses is an index in the computer array.
+        for (var i=0; i < computerChoice.length; i++) {
+            if (computerChoice[i] === userText) {
+                correctArray.push(i);
+                blanksGuess[i] = userText;
+                blanksGuessStr = blanksGuess.join(" ");
+                document.getElementById("blanks").innerHTML = blanksGuessStr;
+            }
+        }
 
-    // If the computer word array contains the user's key value, run this code
-    if (computerArray.includes(userText)) {
-        // In the DOM, grab the element by the id of blanks and set it to the userText.
-        document.getElementById("blanks").innerHTML = userText;
-    }
+    // for (var i=0; i < computerChoice.length; i++) {
+    //     if (computerArray.indexOf(userText) > -1) {
+    //         guessIndex = computerChoice.indexOf(userText);
+    //         blanksGuess[guessIndex] = userText;
+    //         blanksGuessStr = blanksGuess.join(" ");
+    //         document.getElementById("blanks").innerHTML = blanksGuessStr;
+    //         }
+    //     else {
+    //         console.log("it went to else");
+    //     }
+    // }
+
+
+    console.log(guessIndex);
+    
+    console.log(blanksGuessStr);
 
 }
 
@@ -54,8 +77,27 @@ document.onkeyup = function(event) {
 
 // Log the computer array and then log the word with blanks inserted.
     console.log(computerArray);
-    console.log(computerArray.fill("_"));
+    // console.log(computerArray.fill("_"));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("wins").innerHTML = guesses_remaining;
+// let letterBlanks = document.getElementById("blanks");
+// document.getElementById("blanks").innerHTML = "Test";
+
+// When the window loads, set the number of blanks
 
 // Method that breaks the word/string into an array of its letters
 // Underscores that show the letters from the word
